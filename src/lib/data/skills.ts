d5e3cfa8516150781825ee1213de6d181982070c
Martin Můčka <martin.mucka12@google.com>
2024-07-03 10:22:54 +0200

    Corrected c#

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -36,7 +36,7 @@ const defineSkill = <S extends string>(
 
 export const items = [
 	defineSkill({
-		slug: 'c#',
+		slug: 'csharp',
 		color: 'purple',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
