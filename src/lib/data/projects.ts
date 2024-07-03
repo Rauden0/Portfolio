abd5486fff2d8909387308a9aed3d18adcae488a
Martin Můčka <martin.mucka12@google.com>
2024-07-03 12:05:14 +0200

    Added description

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -23,11 +23,11 @@ export const items: Array<Project> = [
 		slug: 'tournament-manager',
 		color: '#ff3e00',
 		description:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
+			'I built a pretty neat Qt-based HTTP server that works as an RSS proxy. It grabs RSS feeds from different sources and sends them to clients in a nice, tidy way. The server can handle lots of client requests at the same time, thanks to Qt’s threading, so it stays fast and responsive. I made sure it runs smoothly with proper synchronization to keep everything in check when multiple threads are doing their thing. Plus, its scalable, so it can grow with demand. Clients can even set their own filters to get just the RSS content they want. My Experience Working on this project was shortly an experience. The framework itself is quite pleasant but the talkativeness of c++ not so much. I really enjoyed the ease of usage of the Qt threads compared to std threads. Also his project really boosted my app development skills especially usage of c++ specific things and network communication.',
 		shortDescription:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
+			'A Vercel-like developer 	 website template made with Typescript and SvelteKit.',
 		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
-		logo: Assets.Svelte,
+		logo: Assets.ExpressJs,
 		name: 'Tournament manager',
 		period: {
 			from: new Date()
